5a38eba5ec2341dba02488c25195fee8608be6c7
tuananh2403 <tuananh240399@gmail.com>
2023-05-16 16:25:31 +0700

    #2 hash password and handle err

--- server/index.js
+++ server/index.js
@@ -2,11 +2,15 @@ const express = require("express");
 
 const app = express();
 const port = process.env.PORT || 3000;
+const initRoutes = require("./routes");
 
+const dbConnect = require("./config/dbconnect");
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
+dbConnect();
+initRoutes(app);
 app.use("/", (req, res) => {
   res.send("TRan tuan anh");
 });
---
+++ server/routes/user.js
@@ -0,0 +1,5 @@
+const router = require("express").Router();
+const ctrls = require("../controllers/user");
+router.post("/register", ctrls.register);
+
+module.exports = router;
