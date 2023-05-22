d1bb65239f458f1a3ed2aecc6445c8d895c2f1ca
tuananh2403 <tuananh240399@gmail.com>
2023-05-22 17:42:00 +0700

    filtering and sorting product

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -1,3 +1,4 @@
+const { response } = require("express");
 const Product = require("../models/product");
 const asyncHandler = require("express-async-handler");
 const slugify = require("slugify");
@@ -19,11 +20,35 @@ const getProduct = asyncHandler(async (req, res) => {
     productData: product ? product : "cannot get product",
   });
 });
+
 const getProducts = asyncHandler(async (req, res) => {
-  const products = await Product.find();
-  return res.status(200).json({
-    success: products ? true : false,
-    productsData: products ? products : "cannot get products",
+  const queries = { ...req.query };
+  const excludeFields = ["limit", "sort", "page", "fields"];
+  excludeFields.forEach((el) => delete queries[el]);
+  let queryString = JSON.stringify(queries);
+  queryString = queryString.replace(
+    /\b(gte|gt|lt|lte)\b/g,
+    (macthedEl) => `$${macthedEl}`
+  );
+  const formatedQueries = JSON.parse(queryString);
+  // filtering
+
+  if (queries?.title)
+    formatedQueries.title = { $regex: queries.title, $options: "i" };
+  let queryCommand = Product.find(formatedQueries);
+  // sorting
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(",").join(" ");
+    queryCommand = queryCommand.sort(sortBy);
+  }
+  queryCommand.exec(async (err, response) => {
+    if (err) throw new Error(err.message);
+    const counts = await Product.find(formatedQueries).countDocuments();
+    return res.status(200).json({
+      success: response ? true : false,
+      productsData: response ? response : "cannot get products",
+      counts,
+    });
   });
 });
 const updateProduct = asyncHandler(async (req, res) => {
