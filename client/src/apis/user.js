b5681b8c99e4dc8009fccf39a98605428741b477
tuananh2403 <tuananh240399@gmail.com>
2023-06-24 12:28:45 +0700

    ui public

---
+++ client/src/apis/user.js
@@ -0,0 +1,39 @@
+import axios from "../axios";
+
+export const apiRegister = (data) =>
+  axios({
+    url: "/user/register",
+    method: "post",
+    data,
+  });
+
+export const apiLogin = (data) =>
+  axios({
+    url: "/user/login",
+    method: "post",
+    data,
+  });
+
+export const apiForgotPassword = (data) =>
+  axios({
+    url: "/user/forgotpassword",
+    method: "post",
+    data,
+  });
+export const apiResetPassword = (data) =>
+  axios({
+    url: "/user/resetpassword",
+    method: "put",
+    data,
+  });
+export const apiCurrent = () =>
+  axios({
+    url: "/user/current",
+    method: "get",
+  });
+export const apiAddCart = (data) =>
+  axios({
+    url: "/user/cart",
+    method: "put",
+    data,
+  });
---
+++ client/src/ultils/contants.js
@@ -0,0 +1,48 @@
+import path from "./path";
+export const navigation = [
+  {
+    id: 1,
+    value: "Trang Chủ",
+    path: ``,
+  },
+  {
+    id: 2,
+    value: "Sản Phẩm",
+    path: `/${path.PRODUCTS}`,
+  },
+  {
+    id: 3,
+    value: "Giới Thiệu",
+    path: `/${path.INTRODUCTION}`,
+  },
+  {
+    id: 4,
+    value: "Thông tin Thuốc",
+    path: `/${path.FAQS}`,
+  },
+  {
+    id: 5,
+    value: "Tin Tức",
+    path: `/${path.BLOGS}`,
+  },
+  {
+    id: 6,
+    value: "Trợ Giúp",
+    path: `/${path.FAQS}`,
+  },
+  {
+    id: 7,
+    value: "Liên Hệ",
+    path: `/${path.FAQS}`,
+  },
+  {
+    id: 8,
+    value: "Chuyên Gia",
+    path: `/${path.FAQS}`,
+  },
+  {
+    id: 9,
+    value: "Báo chí nói về chúng tôi",
+    path: `/${path.FAQS}`,
+  },
+];
